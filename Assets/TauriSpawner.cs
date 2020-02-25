23f9c71ef1362154b5e24ae9e8a977a427b4583e
Liam Mc Cormick <ljmc2000@gmail.com>
2020-02-25 12:33:57 +0000

    Added jets to the tau'ri arsenal

--- Assets/TauriSpawner.cs
+++ Assets/TauriSpawner.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ShipClass{prometheus,drone};
+public enum ShipClass{prometheus,jet};
 
 public struct Ship
 {
@@ -12,33 +12,37 @@ public struct Ship
 
 public class TauriSpawner : MonoBehaviour
 {
+	public int jetCount=0;
+	public float minJetOffset=150;
+	public float maxJetOffset=200;
+
 	public GameObject camera;
-	uint cameraTarget=0;
+	int cameraTarget=-1;
 
 	public List<Ship> ships=new List<Ship>();
 
 	public GameObject Prometheus;
-	public GameObject Drone;
+	public GameObject Jet;
 
 	public Dictionary<ShipClass,Vector3> cameraParams=
 		new Dictionary<ShipClass,Vector3>
 	{
-		{ShipClass.prometheus,new Vector3(0,0,-800)},
-		{ShipClass.drone,new Vector3(0,0,-20)}
+		{ShipClass.prometheus,new Vector3(0,200,-750)},
+		{ShipClass.jet,new Vector3(0,15,-40)}
 	};
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		addShip(ShipClass.prometheus);
-		//addShip(ShipClass.drone);
+		spawnShips();
 		changeSpectateTarget(true);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-
+		if(Input.GetKeyDown("left")) changeSpectateTarget(false);
+		if(Input.GetKeyDown("right")) changeSpectateTarget(true);
 	}
 
 	void changeSpectateTarget(bool up)
@@ -46,16 +50,16 @@ public class TauriSpawner : MonoBehaviour
 		if(up) cameraTarget++;
 		else cameraTarget--;
 
-		uint s = (uint)ships.Count;
-		Debug.Log(ships.Count);
-		cameraTarget %= s;
+		int s = ships.Count;
+		if(cameraTarget<0) cameraTarget+=s;
+		else cameraTarget %= s;
 
 		Ship ship = ships[(int)cameraTarget];
 		camera.transform.parent=ship.body.transform;
-		camera.transform.position=cameraParams[ship.type];
+		camera.transform.position=cameraParams[ship.type]+ship.body.transform.position;
 	}
 
-	void addShip(ShipClass sc)
+	void addShip(ShipClass sc,Vector3 pos)
 	{
 		Ship ship=new Ship();
 		ship.type=sc;
@@ -66,8 +70,8 @@ public class TauriSpawner : MonoBehaviour
 				ship.body=Instantiate(Prometheus);
 				break;
 
-			case ShipClass.drone:
-				ship.body=Instantiate(Drone);
+			case ShipClass.jet:
+				ship.body=Instantiate(Jet);
 				break;
 
 			default:
@@ -75,6 +79,33 @@ public class TauriSpawner : MonoBehaviour
 				break;
 		}
 
+		ship.body.transform.position=pos;
 		ships.Add(ship);
+	}
+
+	void spawnShips()
+	{
+		addShip(ShipClass.prometheus,transform.position);
+		for(int i=0; i<jetCount; i++)
+		{
+			addShip(ShipClass.jet,getRandomOffset());
+		}
+	}
+
+	Vector3 getRandomOffset()
+	{
+		Vector3 t = transform.position;
+
+		t += new Vector3(
+			Random.Range(-maxJetOffset,maxJetOffset),
+			Random.Range(-maxJetOffset,maxJetOffset),
+			Random.Range(-maxJetOffset,maxJetOffset)
+		);
+
+		if(t.x<0) t.x-=minJetOffset; else t.x+=minJetOffset;
+		if(t.y<0) t.y-=minJetOffset; else t.y+=minJetOffset;
+		if(t.z<0) t.z-=minJetOffset; else t.z+=minJetOffset;
+
+		return t;
 	}
 }
