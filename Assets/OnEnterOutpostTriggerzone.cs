7c3d4488e35ef59598b4f9018c1764c772d6a96f
Liam McCormick <ljmc2000@gmail.com>
2020-05-18 00:03:16 +0100

    the sg1 ship will now lazer the ground

---
+++ Assets/OnEnterOutpostTriggerzone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnEnterOutpostTriggerzone : MonoBehaviour
+{
+	public void OnTriggerEnter(Collider collider)
+	{
+		if(collider.CompareTag("outpost_triggerzone"))
+		{
+			GetComponent<LineRenderer>().enabled=true;
+		}
+	}
+}
