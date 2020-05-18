395940a126ffe382dad53743cb8e58e1d47afe6b
Liam McCormick <ljmc2000@gmail.com>
2020-05-18 22:38:54 +0100

    jets, alkesh and the sg1 transport ship are now all fighters

---
+++ Assets/Fighter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : Boid
+{
+	public GameObject missile;
+	public int hitpoints;
+	public int ammunition;
+	public int time_between_shots;
+
+	bool reloading=false;
+
+	void Start()
+	{
+		base.Start();
+	}
+	
+	public void shoot(GameObject target)
+	{
+		if(reloading) return;
+
+		GameObject missile1=Instantiate(missile);
+		missile.GetComponent<Chase>().target=target;
+		ammunition--;
+		StartCoroutine(cooldown());
+	}
+
+	IEnumerator cooldown()
+	{
+		reloading=true;
+		yield return new WaitForSeconds(time_between_shots);
+		reloading=false;
+	}
+}
