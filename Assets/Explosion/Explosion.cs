5b50231a59841f838934800e353337b3a5b3835c
Liam McCormick <ljmc2000@gmail.com>
2020-05-19 18:09:32 +0100

    added explosions

---
+++ Assets/Explosion/Explosion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+	public float size=2;
+	public float rate=10;
+
+	// Update is called once per frame
+	void Update()
+	{
+		float growth=rate*Time.deltaTime;
+		transform.localScale += new Vector3(rate,rate,rate);
+		if(size<transform.localScale.y) Destroy(gameObject);
+	}
+}
